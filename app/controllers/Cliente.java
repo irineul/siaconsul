9da77548a1e8c6a0981deafdf6083caaff5669dc
irineulicks <jrlicks@gmail.com>
2013-05-07 16:29:52 -0300

    Modificação da inclusão de cliente.

--- app/controllers/Cliente.java
+++ app/controllers/Cliente.java
@@ -3,21 +3,40 @@ package controllers;
 import play.mvc.*;
 
 import models.*;
+import play.data.validation.Validation;
 
 public class Cliente extends Controller {
 
+	
+	public static void index(){
+		render();
+	}
+	
+	
 	public static void incluir(String nome, String endereco, String email, String telResidencial, String celular, String rg, String cpfCnpj) {
-		UsuarioModel u = new UsuarioModel();
-		u.setNome(nome);
-		u.setEmail(email);
-		u.setCpfCnpj(cpfCnpj.replaceAll(".", "").replaceAll("-", "").replaceAll("/", ""));
-		u.setTelResidencial(telResidencial);
-		u.setRg(rg);
-		u.setCelular(celular);
-		u.setEndereco(endereco);
-		ClienteModel c = new ClienteModel();
-		c.salvaCliente(u, Consultor.getConsultorLogado());
-        Application.consultor();
-    }
-    
+		validation.required(nome);
+		if(validation.hasErrors()){
+	        params.flash(); // add http parameters to the flash scope
+	        validation.keep(); // keep the errors for the next request			
+			index();
+		}
+		else{
+			UsuarioModel u = new UsuarioModel();
+			u.setNome(nome);
+			u.setEmail(email);
+			String newCpf = cpfCnpj.replace(".", "");
+			newCpf = newCpf.replaceAll("-", "");
+			newCpf = newCpf.replaceAll("/", "");
+			u.setCpfCnpj(newCpf);
+			u.setTelResidencial(telResidencial);
+			u.setRg(rg);
+			u.setCelular(celular);
+			u.setEndereco(endereco);			
+			ClienteModel c = new ClienteModel();
+			c.salvaCliente(u, Consultor.getConsultorLogado());
+			Application.consultor();
+		}
+		render(nome,endereco,email, telResidencial,celular,rg,cpfCnpj);
+	}
+
 }
