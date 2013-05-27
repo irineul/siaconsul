1de7c66e5a3a6b0d8f2749a9214706eae66d0d23
irineulicks <jrlicks@gmail.com>
2013-05-27 18:35:49 -0300

    Nova grid.

---
+++ public/javascripts/DT_bootstrap.js
@@ -0,0 +1,17 @@
+
+$(document).ready(function() {
+    $('#clientesConsultor').dataTable( {
+    	"oLanguage": {
+			"sLengthMenu": "Apresentar _MENU_ registros por página",
+			"sZeroRecords": "Nenhum registro encontrado",
+			"sInfo": "Apresentado _START_ a _END_ de _TOTAL_ registros",
+			"sInfoEmpty": "Apresentado 0 a 0 de 0 records",
+			"sInfoFiltered": "(filtrado de _MAX_ registros)",
+			"sSearch" : "Buscar:",
+			"sPrevious" : "Anterior",
+			"sNext" : "Próximo"
+		},    	
+		"sDom": "<'row'<'span6'l><'span6'f>r>t<'row'<'span6'i><'span6'p>>",
+		"sPaginationType": "bootstrap"
+    } );
+} );
